6543ea37539b6fd012f538fbf387731057c7ddbf
Meisha <meishafifah@students.amikom.ac.id>
2025-05-22 10:18:50 +0700

    feat: change api login in authstore

--- src/stores/auth.store.js
+++ src/stores/auth.store.js
@@ -5,7 +5,7 @@ import { axiosWrapper } from '@/helper/axios-wrapper.js';
 import router from '@/router/index.js';
 
 const baseUrl = import.meta.env.VITE_BASE_URL;
-const loginUrl = `${baseUrl}/api/v1/login`
+const loginUrl = `https://mas-pos.appmedia.id/api/v1/login`
 
 export const useAuthStore = defineStore({
     id: 'auth',
